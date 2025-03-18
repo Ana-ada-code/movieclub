1e98dcfd48b5ddafdc3f61e83cfb8d5e0913a8f7
Ana <adamikana@gmail.com>
2025-03-18 11:15:31 +0100

    initial commit

---
+++ src/main/java/pl/javastart/movieclub/Movieclub2Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.movieclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movieclub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movieclub2Application.class, args);
+	}
+
+}
